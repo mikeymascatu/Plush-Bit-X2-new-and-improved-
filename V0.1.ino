6ab5636dd401af0fed44a772840fc69075ef69a4
mikeymascatu <166886630+mikeymascatu@users.noreply.github.com>
2024-07-05 20:28:53 -0400

    Update V0.1.ino

--- V0.1.ino
+++ V0.1.ino
@@ -15,6 +15,17 @@ Adafruit_Microbit_Matrix microbit;
 // Initialize the OLED display with I2C address 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+int step = 1;
+int mode = 1;
+int buttonA_count = 0;
+int buttonB_count = 0;
+int buttonA_val = 0;
+int buttonB_val = 0;
+
+int ButtonA = 0;
+int ButtonB = 0;
+
+
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
@@ -27,6 +38,7 @@ void setup() {
 
   pinMode(PIN_BUTTON_A, INPUT);
   pinMode(PIN_BUTTON_B, INPUT);
+
 
   display.clearDisplay();
   microbit.begin();
@@ -41,7 +53,6 @@ void setup() {
   display.println(F("Plush"));
   display.setCursor(0, 30);
   display.println(F(":Bit"));
-
   microbit.show(microbit.HEART);
 
   // Update the display with the new content
@@ -52,19 +63,48 @@ void setup() {
   display.clearDisplay();
   display.display();
 
-  display.setCursor(0, 0);
-  display.setTextSize(3);
-  display.println(F("> Math"));
-  display.setCursor(0, 30);
-  display.setTextSize(2);
-  display.println(F("A to Start"));
-  display.setCursor(0, 47);
-  display.setTextSize(2);
-  display.println(F("B to Move"));
-  display.display();
+
 }
 
 void loop() {
 
-  delay(2000);
+  ButtonA = ! digitalRead(PIN_BUTTON_A);
+  ButtonB = ! digitalRead(PIN_BUTTON_B);
+
+  if (ButtonB == 1) {
+    // turn LED on:
+      delay(3000);
+     if(ButtonB == 1) {
+        buttonB_count = buttonB_count + 1;
+     }
+  }
+
+  if (buttonB_count % 2 == 0) { //Checks if the toggle var is even 
+    // turn LED on:
+   display.setCursor(0, 0);
+   display.setTextSize(3);
+   display.println(F("> Math"));
+   display.setCursor(0, 30);
+   display.setTextSize(2);
+   display.println(F("A to Start"));
+   display.setCursor(0, 47);
+   display.setTextSize(2);
+   display.println(F("B to Move"));
+   display.display();
+
+  } else {
+    // turn LED off:
+   display.setCursor(0, 0);
+   display.setTextSize(3);
+   display.println(F("> Read"));
+   display.setCursor(0, 30);
+   display.setTextSize(2);
+   display.println(F("A to Start"));
+   display.setCursor(0, 47);
+   display.setTextSize(2);
+   display.println(F("B to Move"));
+   display.display();
+  } 
+  delay(100);
 }
+
