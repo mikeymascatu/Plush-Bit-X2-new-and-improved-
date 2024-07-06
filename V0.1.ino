0ff900e82255794bcb0f78ccb361b5cb1511c0c4
mikeymascatu <166886630+mikeymascatu@users.noreply.github.com>
2024-07-05 23:01:10 -0400

    Update V0.1.ino

--- V0.1.ino
+++ V0.1.ino
@@ -15,9 +15,12 @@ Adafruit_Microbit_Matrix microbit;
 // Initialize the OLED display with I2C address 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-int step = 1;
-int mode = 1;
-int buttonA_count = 0;
+
+ 
+int stage = 1;
+int mode = 0;
+int digit_num = 0;
+int buttonA_count = 1;
 int buttonB_count = 0;
 int buttonA_val = 0;
 int buttonB_val = 0;
@@ -38,7 +41,6 @@ void setup() {
 
   pinMode(PIN_BUTTON_A, INPUT);
   pinMode(PIN_BUTTON_B, INPUT);
-
 
   display.clearDisplay();
   microbit.begin();
@@ -73,24 +75,51 @@ void loop() {
 
   if (ButtonB == 1) {
     // turn LED on:
-      delay(3000);
+      delay(100);
      if(ButtonB == 1) {
         buttonB_count = buttonB_count + 1;
+        Serial.write(buttonB_count);
+        display.clearDisplay();  
      }
   }
 
-  if (buttonB_count % 2 == 0) { //Checks if the toggle var is even 
+  if (ButtonA == 1) {
     // turn LED on:
-   display.setCursor(0, 0);
-   display.setTextSize(3);
-   display.println(F("> Math"));
-   display.setCursor(0, 30);
-   display.setTextSize(2);
-   display.println(F("A to Start"));
-   display.setCursor(0, 47);
-   display.setTextSize(2);
-   display.println(F("B to Move"));
-   display.display();
+    delay(100);
+    if(ButtonA == 1) {
+        buttonA_count = buttonA_count + 1;
+        Serial.write(buttonA_count);
+        display.clearDisplay();  
+    }
+  }
+
+  if (stage == 1){
+    if (buttonB_count % 2 == 0) { //Checks if the toggle var is even 
+      // turn LED on:
+    display.setCursor(0, 0);
+    display.setTextSize(3);
+    display.println(F("> Math"));
+    display.setCursor(0, 30);
+    display.setTextSize(2);
+    display.println(F("A to Start"));
+    display.setCursor(0, 47);
+    display.setTextSize(2);
+    display.println(F("B to Move"));
+    display.display();
+   
+  if (buttonA_count % 2 == 0) { //Checks if the toggle var is even 
+    // turn LED on:
+    
+    stage = 2;
+    mode = 1;
+    delay(10);
+    display.clearDisplay();
+    display.display();
+    delay(10);
+
+  } else {
+    // turn LED off:
+  } 
 
   } else {
     // turn LED off:
@@ -105,6 +134,36 @@ void loop() {
    display.println(F("B to Move"));
    display.display();
   } 
+  if(stage == 2){
+    if(mode == 1){
+      if (buttonB_count % 2 == 0) {
+        display.setCursor(0, 0);
+        display.setTextSize(3);
+        display.println(F("Single"));
+        display.setCursor(0, 30);
+        display.setTextSize(2);
+        display.println(F("A to Start"));
+        display.setCursor(0, 47);
+        display.setTextSize(2);
+        display.println(F("B to Move"));
+        display.display();
+      } else {
+        display.setCursor(0, 0);
+        display.setTextSize(3);
+        display.println(F("Double"));
+        display.setCursor(0, 30);
+        display.setTextSize(2);
+        display.println(F("A to Start"));
+        display.setCursor(0, 47);
+        display.setTextSize(2);
+        display.println(F("B to Move"));
+        display.display();
+      }
+    }
+  }
   delay(100);
 }
+
+}
+
 
